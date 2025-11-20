0e3110616eb87afc1b39a8be07ce387f175e94f5
pirosiki197 <lingmukangtai825@gmail.com>
2025-11-20 21:59:29 +0900

    Fix: Handle unexpected NaNs

--- src/color.rs
+++ src/color.rs
@@ -8,9 +8,11 @@ pub fn write_color(w: &mut impl Write, mut pixel_color: Color, samples_per_pixel
     // gamma correction
     pixel_color = pixel_color.sqrt();
 
-    let r = 256.0 * pixel_color.x().clamp(0.0, 0.999);
-    let g = 256.0 * pixel_color.y().clamp(0.0, 0.999);
-    let b = 256.0 * pixel_color.z().clamp(0.0, 0.999);
+    let sanitize = |v: f32| if v.is_nan() { 0.0 } else { v }.clamp(0.0, 0.999);
+
+    let r = 256.0 * sanitize(pixel_color.x());
+    let g = 256.0 * sanitize(pixel_color.y());
+    let b = 256.0 * sanitize(pixel_color.z());
 
     write!(w, "{} {} {}\n", r as u8, g as u8, b as u8).unwrap();
 }
